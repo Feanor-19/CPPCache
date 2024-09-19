f46730bf69aabf6454d908a04c2f1f0199974ddb
Filipp <sap.phil.v@mail.ru>
2024-09-19 22:57:43 +0300

    first compiled version

--- inc/ideal_cache.hpp
+++ inc/ideal_cache.hpp
@@ -11,17 +11,64 @@ class IdealCache
     size_t size_;
 
     std::vector<T> cache_;
-    using CacheIt = std::vector<T>::iterator;
+    using CacheIt = typename std::vector<T>::iterator;
 
     std::unordered_map<KeyT, CacheIt> hash_;
 
     const std::vector<KeyT> &queries_; 
-    using QueriesIt = std::vector<KeyT>::const_iteraror;
-    QueriesIt cur_queries_it;
+    using QueriesIt = typename std::vector<KeyT>::const_iterator;
+    QueriesIt cur_plus_one_queries_it_;
 
 public:
-    IdealCache(size_t cache_size, const std::vector<KeyT> &queries);
+    explicit IdealCache(size_t cache_size, const std::vector<KeyT> &queries);
 
     template <typename F>
     bool lookup_update(KeyT id, F slow_get_page);
 };
+
+#include <algorithm>
+
+template <typename T, typename KeyT>
+IdealCache<T, KeyT>::IdealCache(size_t cache_size, const std::vector<KeyT> &queries):
+    cache_(cache_size), queries_(queries)
+{
+    cache_.reserve(cache_size);
+    cur_plus_one_queries_it_ = queries_.cbegin();
+}
+
+template <typename T, typename KeyT>
+template <typename F>
+bool IdealCache<T, KeyT>::lookup_update(KeyT id, F slow_get_page)
+{
+    cur_plus_one_queries_it_++;
+
+    if (hash_.find(id) != hash_.end())
+        return true;
+
+    // if cache is not full yet
+    size_t cur_size = 0;
+    if ( (cur_size = hash_.size()) != size_)
+    {
+        cache_[cur_size] = slow_get_page(id);
+        hash_[id] = cache_.begin() + cur_size;
+        return false;
+    }
+
+    std::unordered_set<KeyT> unmet_ids;
+    for (auto it = hash_.cbegin(); it != hash_.cend(); ++it) 
+        unmet_ids.insert(it->first);
+
+    auto id_to_pop_it = cur_plus_one_queries_it_;
+    for (; id_to_pop_it != queries_.cend() && unmet_ids.size() > 1; id_to_pop_it++)
+        unmet_ids.erase(*id_to_pop_it);
+
+    const KeyT id_to_pop = *(unmet_ids.cbegin());
+
+    CacheIt cache_it = hash_[id_to_pop];
+    *cache_it = slow_get_page(id);
+
+    hash_.erase(id_to_pop);
+    hash_[id] = cache_it;
+
+    return false;
+}
--- src/ICmain.cpp
+++ src/ICmain.cpp
@@ -1,12 +1,36 @@
+#include "ideal_cache.hpp"
+
 #include <iostream>
 
-int main(int, char**){
+struct page_t
+{
+    int id;
+    void *data;
+};
 
-    int a = 0;
+page_t slow_get_page(int id)
+{
+    // it should be slow on its own?
+    return {id, NULL};
+}
 
-    a = a + 19;
+int main(int, char**)
+{
+    size_t cache_size = 0, elems_cnt = 0;
+    std::cin >> cache_size >> elems_cnt;
 
-    std::cout << a << std::endl;
+    std::vector<int> queries;
+    queries.reserve(elems_cnt);
 
-    std::cout << "Hello, from FirstTaskCache!\n";
+    int page_id = 0;
+    for (size_t i = 0; i != elems_cnt; i++)
+        std::cin >> queries[i];
+    
+    IdealCache<page_t> cache(cache_size, queries);
+    
+    size_t hits = 0;
+    for (auto it = queries.cbegin(); it != queries.cend(); it++)
+        hits += cache.lookup_update(*it, slow_get_page);
+
+    std::cout << hits;
 }
