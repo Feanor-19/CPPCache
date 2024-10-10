9b2264dc95968a690eebce6e8938b3db6476b8da
Filipp <sap.phil.v@mail.ru>
2024-10-10 13:16:06 +0300

    finally benchmark accepts .dat file name as an arg

--- LFUCache/src/benchmark.cpp
+++ LFUCache/src/benchmark.cpp
@@ -16,12 +16,17 @@ page_t slow_get_page(int id)
     return {id, NULL};
 }
 
-static void BM_LFUC_lookup_update(benchmark::State &state)
+template <class ...Args>
+static void BM_LFUC_lookup_update(benchmark::State &state, Args&&... args)
 {
+    //benchmark arg setup
+    auto args_tuple = std::make_tuple(std::move(args)...);
+    std::string bench_data_filename = std::get<0>(args_tuple); 
+
     //setup
     size_t cache_size = 0, queries_cnt = 0;
 
-    std::ifstream file("benchmark_data/bench_1000_100000_10000.dat");
+    std::ifstream file(bench_data_filename);
     if (!file.is_open())
         std::cout << "Unable to open file" << std::endl; 
 
@@ -34,11 +39,11 @@ static void BM_LFUC_lookup_update(benchmark::State &state)
     while (queries_cnt-- && file >> input)
         queries.push_back(input);
 
-    LFUCache<page_t> cache(cache_size);
     size_t hits = 0;
     for (auto _ : state)
     {
         //this code is timed
+        LFUCache<page_t> cache(cache_size);
         hits = 0;
         for (auto it = queries.cbegin(); it != queries.cend(); it++)
         {
@@ -48,6 +53,12 @@ static void BM_LFUC_lookup_update(benchmark::State &state)
     std::cout << "Hits: " << hits << std::endl;
 }
 
-BENCHMARK(BM_LFUC_lookup_update);
+//BENCHMARK(BM_LFUC_lookup_update);
+
+BENCHMARK_CAPTURE(BM_LFUC_lookup_update, 1000_100000_10000, \
+                  std::string("benchmark_data/1000_100000_10000.dat"));
+
+BENCHMARK_CAPTURE(BM_LFUC_lookup_update, problem_lc_015, \
+                  std::string("benchmark_data/problem_lc_015.dat"));
 
 BENCHMARK_MAIN();
